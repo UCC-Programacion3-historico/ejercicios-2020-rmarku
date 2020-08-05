a8abc0483cb003fd3f65d6ac30ccfce801eba24b
Ricardo Martín Marcucci <rmarku@gmail.com>
2020-08-05 11:02:38 -0300

    Implemented potencia Ej01

--- U01_Recursividad/Ej-01/main.cpp
+++ U01_Recursividad/Ej-01/main.cpp
@@ -3,6 +3,9 @@
 #include "potencia.h"
 
 int main() {
+   auto resultado = potencia(2, 3);
+
   std::cout << "Ejercicio 01/01\n" << std::endl;
+  std::cout << resultado << "\n" << std::endl;
   return 0;
 }
--- U01_Recursividad/Ej-01/potencia.cpp
+++ U01_Recursividad/Ej-01/potencia.cpp
@@ -1,2 +1,7 @@
 
-unsigned int potencia(unsigned int base, unsigned int exp) { return 0; }
+unsigned int potencia(unsigned int base, unsigned int exp) {
+  if (exp == 0) {
+    return 1;
+  }
+  return base * potencia(base, exp - 1);
+}
